edef2b8a1c41f715939af0f364048815d3b48962
Eugene <eugeneayonga@gmail.com>
2022-09-09 17:22:22 +0300

    Created a Home.js component

---
+++ quotex/src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <div>
+                <h1>Home</h1>
+                <p>Welcome to QuoteX! Your inspiration quotes catalogue.</p>
+                <img
+                    alt="Minimally QuoteX"
+                    src="https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/4k/preview/05.jpg?3"
+                />
+            </div>
+        </>
+    );
+};
+
+export default Home;
