0ffe6912073828a8800e18846e40c39dbe2a393d
Eugene <eugeneayonga@gmail.com>
2022-09-09 17:30:45 +0300

    Built NavBar.js component

---
+++ quotex/src/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+    return (
+      <>
+        <div className="navbar">
+          <NavLink to="/">
+            <button className="button">Home</button>
+          </NavLink>
+          <NavLink to="/quotes">
+            <button className="button">Quotes</button>
+          </NavLink>
+          <NavLink to="/formpage">
+            <button className="button">Add New Quotes</button>
+          </NavLink>
+        </div>
+      </>
+    );
+}
+
+export default NavBar;
